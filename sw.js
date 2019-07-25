aaaac79fe420b31460b36e29b9650d0715a95606
Julia Crosara <47763769+juliacrosara@users.noreply.github.com>
2019-07-24 21:12:08 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 self.addEventListener('install', function(e) {
     // Waits until install event is complete
     e.waitUntil(
-        // opens caches object and returns a promise chained
+        // Opens caches object and returns a promise chained
         // with a function that returns added cacheFiles
         caches.open('v1').then(function(cache) {
             return cache.addAll(cacheFiles);
@@ -10,7 +10,7 @@ self.addEventListener('install', function(e) {
     );
 });
 
-// caches an array of file path strings that the app uses/requests
+// Caches an array of file path strings that the app uses/requests
 const cacheFiles = [
     '/',
     '/index.html',
@@ -34,25 +34,28 @@ const cacheFiles = [
 
 // Listens for fetch addEvent
 self.addEventListener('fetch', function(e) {
-    //respondWith method prevents default fetch event
+    // respondWith method prevents default fetch event
     e.respondWith(
         // match method checks if event request url is already in cacheFiles
         // loaded during install. Chain then method to receive a promise.
         caches.match(e.request).then(function(response) {
-            // check if there's a match
+            // check if there's a match; if true, returns request
             if (response) {
                 console.log('Found ', e.request, ' in cache');
                 return response;
             } else {
                 console.log('Could not find ', e.request, ' in cache, FETCHING');
+                // if false, the request is fetched and added to the cache
                 return fetch(e.request)
                     .then(function(response) {
                         const clonedResponse = response.clone();
+                        // open cache; use put method to pair request & response
                         caches.open('v1').then(function(cache) {
                             cache.put(e.request, clonedResponse);
-                        })
+                        });
                         return response;
                     })
+                    // catch method logs potential errors
                     .catch(function(err) {
                         console.error(err);
                     });
