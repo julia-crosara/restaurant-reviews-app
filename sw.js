c5076d0d59e72b601be32867e14d6d074cb69604
Julia Crosara <47763769+juliacrosara@users.noreply.github.com>
2019-07-24 20:10:13 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,62 @@
+// Listens for install event which fires when sw is registered
+self.addEventListener('install', function(e) {
+    // Waits until install event is complete
+    e.waitUntil(
+        // opens caches object and returns a promise chained
+        // with a function that returns added cacheFiles
+        caches.open('v1').then(function(cache) {
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+
+// caches an array of file path strings that the app uses/requests
+const cacheFiles = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg',
+];
+
+// Listens for fetch addEvent
+self.addEventListener('fetch', function(e) {
+    //respondWith method prevents default fetch event
+    e.respondWith(
+        // match method checks if event request url is already in cacheFiles
+        // loaded during install. Chain then method to receive a promise.
+        caches.match(e.request).then(function(response) {
+            // check if there's a match
+            if (response) {
+                console.log('Found ', e.request, ' in cache');
+                return response;
+            } else {
+                console.log('Could not find ', e.request, ' in cache, FETCHING');
+                return fetch(e.request)
+                    .then(function(response) {
+                        const clonedResponse = response.clone();
+                        caches.open('v1').then(function(cache) {
+                            cache.put(e.request, clonedResponse);
+                        })
+                        return response;
+                    })
+                    .catch(function(err) {
+                        console.error(err);
+                    });
+            }
+        })
+    );
+});
