e36b917308e7770e115187e5aa04a051952d244e
Julia Crosara <47763769+juliacrosara@users.noreply.github.com>
2019-08-06 12:33:02 -0700

    final edits

--- sw.js
+++ sw.js
@@ -51,11 +51,7 @@ self.addEventListener('fetch', function(event) {
                     if (!response || response.status !== 200 || response.type !== 'basic') {
                         return response;
                     }
-
-                    // IMPORTANT: Clone the response. A response is a stream
-                    // and because we want the browser to consume the response
-                    // as well as the cache consuming the response, we need
-                    // to clone it so we have two streams.
+                    // Clone the response 
                     var responseToCache = response.clone();
 
                     caches.open(CACHE_NAME)
